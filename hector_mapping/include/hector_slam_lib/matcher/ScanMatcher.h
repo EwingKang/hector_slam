8bdb6137c67927589b3cbb89de06a3e196605d34
Ewing Kang <f039281310@yahoo.com.tw>
2018-01-27 17:26:20 +0800

    Some modification

--- hector_mapping/include/hector_slam_lib/matcher/ScanMatcher.h
+++ hector_mapping/include/hector_slam_lib/matcher/ScanMatcher.h
@@ -206,11 +206,11 @@ protected:
 
       //std::cout << "\nsearchdir\n" << searchDir  << "\n";
 
-      if (searchDir[2] > 0.2f) {
-        searchDir[2] = 0.2f;
+      if (searchDir[2] > 0.2f*8) {
+        searchDir[2] = 0.2f*8;
         std::cout << "SearchDir angle change too large\n";
-      } else if (searchDir[2] < -0.2f) {
-        searchDir[2] = -0.2f;
+      } else if (searchDir[2] < -0.2f*8) {
+        searchDir[2] = -0.2f*8;
         std::cout << "SearchDir angle change too large\n";
       }
 
